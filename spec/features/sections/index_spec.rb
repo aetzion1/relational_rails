e770b224f125b1b9d85e49139a691842a5f50b9a
Adam Etzion <adametzion90@gmail.com>
2020-12-03 15:54:17 -0700

    Add User Story 1 test, successfully

--- spec/features/sections/index_spec.rb
+++ spec/features/sections/index_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Show name of each section' do
     
-    it  
+    it  'Can show section names' do
+        clarinet = Section.create!(
+            name: 'Clarinet', 
+            created_date: '2005-01-01',
+            section_full: false
+        )
+        violin = Section.create!(
+            name: 'Violin', 
+            created_date: '2000-03-29',
+            section_full: true
+        )
+
+        visit "/sections/"
+
+        expect(page).to have_content(clarinet.name)
+        expect(page).to have_content(violin.name)
+    end
 
 end
