43f1cd540c1735ea96eec0befd5c223b061b5c6a
Robert Heath <robert.b.heathii@gmail.com>
2020-12-04 18:30:57 -0600

    Complete code for User Story 3

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -9,20 +9,20 @@ class SectionsController < ApplicationController
   end
 
   def create
-    section = Section.new(section_params)
-    section.save!
+    @section = Section.create!(section_params)
+    #section = Section.new(section_params)
     redirect_to "/sections"
   end
 
   def new
-    @section = Section.new({name: params[:section][:title],
-                           section_full: params[:section][:section_full]
-                           })
+    # @section = Section.new({name: params[:section][:title],
+    #                        section_full: params[:section][:section_full]
+    #                        })
   end
 
 private
 
   def section_params
-    params.permit(:name, :section_full, :play_count)
+    params.permit(:name, :section_full, :created_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/sections', to: 'sections#index'
-  get '/sections/:id', to: 'sections#show'
-  post '/sections', to: 'sections#create'
+  get '/sections/:id', to: 'sections#show', :constraints => { :id => /[0-9]+(\%7C[0-9]+)*/ }
   get '/sections/new', to: 'sections#new'
+  post '/sections', to: 'sections#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
