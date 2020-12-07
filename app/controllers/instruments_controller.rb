4b31acfca53436e702a8b3289f6996f423b0c7a2
Adam Etzion <adametzion90@gmail.com>
2020-12-07 15:58:54 -0700

    Add user story 7

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -5,12 +5,14 @@ class InstrumentsController < ApplicationController
     end
 
     def show
-        @instrument = Instrument.find(params[:id])
+        @section_id = params[:id]
+        @section_name = Section.where(id: @section_id)[0].name
+        @instruments = Instrument.where(section_id: @section_id)
     end
 
-    private
-    def instrument_params
-        params.permit(:name, :date_hired, :age)
-    end
+    # private
+    # def instrument_params
+    #     params.permit(:name, :date_hired, :age)
+    # end
 
 end
--- spec/features/instruments/index_spec.rb
+++ spec/features/instruments/index_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe "Instrument Index" do
                 clarinet = Section.create!(name: 'Clarinet', created_date: '2005-01-01', cartage: false)
                 
                 p_clarinet = Instrument.create!(name: 'Principal', date_hired: '2020-03-29', age: 52, section_id: clarinet.id)
-                # vp_clarinet = Instrument.create!(name: 'Vice Principal', date_hired: '2010-04-15', age: 24, section_id: clarinet.id)
+                vp_clarinet = Instrument.create!(name: 'Vice Principal', date_hired: '2010-04-15', age: 24, section_id: clarinet.id)
 
                 visit "/sections/#{clarinet.id}/instruments"
                 save_and_open_page
