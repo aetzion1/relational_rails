904ff9965bac29f3e43d72b13ccd4c948643107c
Adam Etzion <adametzion90@gmail.com>
2020-12-08 15:52:46 -0700

    Add user story 9 test

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -15,9 +15,21 @@ class InstrumentsController < ApplicationController
         @section_name = Section.where(id: @instrument.section_id)[0].name
     end
 
-    # private
-    # def instrument_params
-    #     params.permit(:name, :date_hired, :age)
-    # end
+    def new
+
+    end
+
+    def create
+      @section = Section.find(params[:id])
+      require 'pry'; binding.pry
+      @section_id = @section.id
+      @instrument = @section.instruments.create!(instrument_params)
+      redirect_to "/sections/#{@section_id}/instruments"
+    end
+
+    private
+    def instrument_params
+        params.permit(:name, :date_hired, :age)
+    end
 
 end
